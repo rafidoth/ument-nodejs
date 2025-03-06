9404b787fdbe57bb29a4d205cdc1c0a595042d52
rafidoth <rafiulhasan803@gmail.com>
2025-03-07 02:52:55 +0600

    express initialization

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = 3010;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
